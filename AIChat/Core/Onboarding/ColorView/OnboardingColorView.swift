b70440b0e4f9e7bdb7e2fc85cf796ad435b8bd57
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2025-02-03 23:19:54 +0900

    [Feature] OnboardingIntroView, OnboardingColorView

---
+++ AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChat
+//
+//  Created by 안다영 on 2/3/25.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
---
+++ AIChat/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  AIChat
+//
+//  Created by 안다영 on 2/3/25.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
